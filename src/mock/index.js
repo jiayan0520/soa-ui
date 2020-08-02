b94e78df43f89252af5af6b53c86f1ecdce4792b
chenjiayan <chenjiayan@ruijie.com.cn>
2020-08-03 00:00:21 +0800

    mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -6,5 +6,8 @@ Mock.setup({ timeout: '350-600' })
 
 // 通用
 Mock.mock(/\/task\/addTask/, 'post', data.fetch)
+Mock.mock(/\/author\/sign/, 'post', data.sign)
+Mock.mock(/\/author\/getUserId/, 'post', data.getUserId)
+Mock.mock(/\/author\/getUserInfo/, 'post', data.getUserInfo)
 
 export default Mock
